6a73832eb2e33a861196ea0fa966bbee392aa546
yurii2007 <yurash862@gmail.com>
2023-09-25 17:54:56 +0300

    add button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import { ButtonStyled } from './Button.styled';
+
+export const Button = ({ text, isGradient }) => {
+    const styles = isGradient ? {
+        background: "var(--btn-gradient-color)",
+        color: 'var(--white-color)'
+  } : {
+    background: "var(--main-font-color)",
+    color: '#623F8B'
+}
+  return <ButtonStyled style={styles}>{text}</ButtonStyled>;
+};

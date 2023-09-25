9623576bdaf9b17829e8122b33128b598e16df6e
yurii2007 <yurash862@gmail.com>
2023-09-25 16:42:48 +0300

    base config

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/moneyguard_project' >
-    <App />
+    <BrowserRouter basename="/moneyguard_project">
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );

904bdea15e2cc58b44631a0fc32c38cc1419aa87
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-09-27 20:11:03 +0300

    додав кнопку фaільтраці

---
+++ src/components/StatisticsModules/components/FilterButton/FilterButton.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+// import { BsCaretDown, BsCaretUp } from 'react-icons/bs';
+import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai';
+import { Button } from './FilterButton.styled';
+
+const FilterButton = ({ handleFilterData }) => {
+  const [toggle, setToggle] = useState(true);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+    handleFilterData(toggle);
+  };
+  return (
+    <Button onClick={handleClick}>
+      {toggle ? (
+        <AiFillCaretDown size="18" fill="white" />
+      ) : (
+        <AiFillCaretUp size="18" fill="white" />
+      )}
+    </Button>
+  );
+};
+
+export default FilterButton;

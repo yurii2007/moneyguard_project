feaa2996de92100b27b60769249612101d8028c2
Yurii <135147654+yurii2007@users.noreply.github.com>
2023-09-27 11:36:29 +0300

    rename selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,3 @@
-export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectIsLoggedIn = state => state.auth.isAuth;
 export const selectUser = state => state.auth.user;
-export const selectIsRefreshing = state => state.auth.isRefreshing;
+export const selectIsRefreshing = state => state.auth.isRefresh;

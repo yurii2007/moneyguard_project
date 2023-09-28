6e9f233302943e2810e0aecc43dd2a6784991b1d
Andrii Krytsia <124679055+AndriiKrytsia@users.noreply.github.com>
2023-09-28 11:21:16 +0200

    1111

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,13 @@ import { ReactComponent as ReactLogo } from '../../images/svg/logo.svg';
 import { ReactComponent as ExitIconButton } from '../../images/svg/exit-icon.svg';
 import { useState } from 'react';
 import { ModalLogout } from 'components/ModalLogout/ModalLogout';
+import { selectUserEmail } from 'redux/selectors';
+import { useSelector } from 'react-redux';
 
 export const Header = () => {
   const [isModalShow, setModalShow] = useState(false);
+  let userEmail = useSelector(selectUserEmail);
+  userEmail = userEmail?.replace(/@.+/gi, '');
 
   return (
     <>
@@ -15,7 +19,7 @@ export const Header = () => {
           <span className="logo-text">Money Guard</span>
         </div>
         <div className="exit-container">
-          <p className="user-name">Name</p>
+          <p className="user-name">{userEmail}</p>
           <div className="divider"></div>
           <button className="exit-button" onClick={() => setModalShow(true)}>
             <ExitIconButton className="exit-button-svg"></ExitIconButton>

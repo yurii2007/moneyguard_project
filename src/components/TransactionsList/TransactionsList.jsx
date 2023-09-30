9c37fdc59c543984cbf52c32660fc4c569286957
Horbunov Yurii <urijnet@gmail.com>
2023-09-30 10:15:59 +0300

    adaptive

--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { StyledTransactionsList } from './TransactionsList.styled';
+import { StyledTransactionsList, Wraper } from './TransactionsList.styled';
 import { TransactionsItem } from 'components/TransactionsList/TransactionsItem/TransactionsItem';
 import {
   createTransactionThunk,
@@ -31,9 +31,8 @@ const TransactionsList = () => {
   };
 
   return (
-    <StyledTransactionsList>
-      <button onClick={onAddTransaction}>Add test transaction</button>
-      {Array.isArray(transactions) && transactions.length > 0 ? (
+    <Wraper>
+      <StyledTransactionsList>
         <table>
           <thead>
             <tr>
@@ -46,25 +45,42 @@ const TransactionsList = () => {
             </tr>
           </thead>
           <tbody>
-            {transactions.map(
-              ({ id, transactionDate, type, categoryId, comment, amount }) => (
-                <TransactionsItem
-                  key={id}
-                  id={id}
-                  transactionDate={transactionDate}
-                  type={type}
-                  categoryId={categoryId}
-                  comment={comment}
-                  amount={amount}
-                />
+            {Array.isArray(transactions) && transactions.length > 0 ? (
+              transactions.map(
+                ({
+                  id,
+                  transactionDate,
+                  type,
+                  categoryId,
+                  comment,
+                  amount,
+                }) => (
+                  <TransactionsItem
+                    key={id}
+                    id={id}
+                    transactionDate={transactionDate}
+                    type={type}
+                    categoryId={categoryId}
+                    comment={comment}
+                    amount={amount}
+                  />
+                )
               )
+            ) : (
+              <p>There are no transactions added!</p>
             )}
           </tbody>
         </table>
-      ) : (
-        <thead>There are no transactions added!</thead>
-      )}
-    </StyledTransactionsList>
+      </StyledTransactionsList>
+
+      <button onClick={onAddTransaction}>
+        <span>+</span>
+        {/* <svg>
+          <use href='../../images/svg/plus.svg'></use>
+        </svg> */}
+        {/* <img src="../../images/svg/plus.svg" alt="" /> */}
+      </button>
+    </Wraper>
   );
 };
 
--- src/components/TransactionsList/TransactionsList.styled.js
+++ src/components/TransactionsList/TransactionsList.styled.js
@@ -1,240 +1,343 @@
 import styled from 'styled-components';
 
+export const Wraper = styled.div`
+  div > div {
+    position: relative;
+  }
+  div + button {
+    color: white;
+    fill: white;
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    border: none;
+    background: var(
+      --button-gradient,
+      linear-gradient(97deg, #ffc727 -16.42%, #9e40ba 97.04%, #7000ff 150.71%)
+    );
+    position: absolute;
+    right: 0;
+    bottom: 0px;
+  }
+  @media only screen and (min-width: 767px) {
+    div + button {
+      bottom: -20px;
+    }
+  }
+
+  @media only screen and (min-width: 1280px) {
+    div + button {
+      bottom: 20px;
+    }
+  }
+`;
+
 export const StyledTransactionsList = styled.div`
   margin: 0 auto;
   position: relative;
   overflow-y: overlay;
 
-  // &.expense::before {
-  //   content: '';
-  //   position: absolute;
-  //   left: 0;
-  //   height: 300px;
-  //   width: 5px;
-  //   background-color: var(--dashboard-text);
-  // }
+  &.expense::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    height: 300px;
+    width: 5px;
+    background-color: var(--dashboard-text);
+  }
 
-  // &.income::before {
-  //   content: '';
-  //   position: absolute;
-  //   left: 0;
-  //   height: 300px;
-  //   width: 5px;
-  //   background-color: var(--yellow);
-  // }
+  &.income::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    height: 300px;
+    width: 5px;
+    background-color: var(--yellow);
+  }
 
-  // li {
-  //   height: 48px;
-  //   display: flex;
-  //   justify-content: space-between;
-  //   align-items: center;
-  //   position: relative;
-  // }
+  li {
+    height: 48px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+  }
 
-  // li:nth-child(4) p {
-  //   text-align: right;
-  // }
+  li:nth-child(4) p {
+    text-align: right;
+  }
 
-  // li:not(:last-child)::after {
-  //   content: '';
-  //   position: absolute;
-  //   left: -20px;
-  //   right: -20px;
-  //   bottom: -1px;
-  //   height: 1px;
-  //   border-bottom: 1px solid white;
-  //   opacity: 0.2;
-  // }
+  li:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    left: -20px;
+    right: -20px;
+    bottom: -1px;
+    height: 1px;
+    border-bottom: 1px solid white;
+    opacity: 0.2;
+  }
 
-  // li:last-child {
-  //   height: 53px;
-  // }
+  li:last-child {
+    height: 53px;
+  }
 
-  // &.expense li:nth-child(5) p {
-  //   font-weight: 600;
-  //   color: var(--dashboard-text);
-  //   text-align: right;
-  // }
+  &.expense li:nth-child(5) p {
+    font-weight: 600;
+    color: var(--dashboard-text);
+    text-align: right;
+  }
 
-  // &.income li:nth-child(5) p {
-  //   font-weight: 600;
-  //   color: var(--yellow);
-  //   text-align: right;
-  // }
+  &.income li:nth-child(5) p {
+    font-weight: 600;
+    color: var(--yellow);
+    text-align: right;
+  }
 
-  // b {
-  //   font-weight: 600;
-  //   margin-right: 27px;
-  // }
+  b {
+    font-weight: 600;
+    margin-right: 27px;
+  }
 
-  // button.edit {
-  //   width: 48px;
-  //   height: 24px;
-  //   background-color: transparent;
-  //   color: var(--white-60);
-  //   box-shadow: none;
-  // }
+  button.edit {
+    width: 48px;
+    height: 24px;
+    background-color: transparent;
+    color: var(--white-60);
+    box-shadow: none;
+  }
 
-  // button.edit:hover {
-  //   color: var(--white);
-  // }
+  button.edit:hover {
+    color: var(--white);
+  }
 
-  // button.delete {
-  //   color: var(--white);
-  //   background: var(--gradient-button);
-  //   width: 69px;
-  //   height: 29px;
-  //   border-radius: 20px;
-  //   font-size: 14px;
-  //   box-shadow: var(--button-shadow);
-  // }
+  button.delete {
+    color: var(--white);
+    background: var(--gradient-button);
+    width: 69px;
+    height: 29px;
+    border-radius: 20px;
+    font-size: 14px;
+    box-shadow: var(--button-shadow);
+  }
 
-  // @media only screen and (max-width: 767px) {
-  //   padding: 0 20px;
-  //   height: 293px;
-  //   margin-bottom: 8px;
-  //   border-radius: 10px;
-  //   height: 293px;
-  //   background: rgba(255, 255, 255, 0.1);
+  @media only screen and (max-width: 767px) {
+    padding: 0 20px;
+    height: 293px;
+    margin-bottom: 8px;
+    border-radius: 10px;
+    height: 293px;
+    background: rgba(255, 255, 255, 0.1);
 
-  //   button.edit {
-  //     display: flex;
-  //     justify-content: space-between;
-  //   }
-  // }
+    table {
+      display: flex;
+    }
 
-  // @media only screen and (min-width: 768px) {
-  //   display: block;
-  //   max-height: 318px;
-  //   overflow: auto;
-  //   width: 704px;
-  //   &::-webkit-scrollbar {
-  //     width: 8px;
-  //     border-radius: 4px;
-  //     background-color: #734aef;
-  //   }
+    // thead tr {
+    //   height: 100%;
+    //   display: flex;
+    //   flex-direction: column;
+    //   justify-content: space-between
+    // }
 
-  //   &::-webkit-scrollbar-thumb {
-  //     background-color: var(--balance-background-color);
-  //     border-radius: 4px;
-  //   }
+    tr {
+      display: flex;
+      flex-direction: column;
+      // outline: solid green;
+      position: relative;
+      padding: 12px 0;
+    }
 
-  //   .income {
-  //     font-weight: 600;
-  //     color: var(--yellow);
-  //     text-align: right;
-  //   }
-  //   .expense {
-  //     font-weight: 600;
-  //     color: var(--dashboard-text);
-  //     text-align: right;
-  //   }
+    tbody tr:nth-child(7) {
+      padding-bottom: 53px;
+    }
+    tbody tr::after {
+      content: '';
+      display: block;
+      height: 1px;
+      width: 150%;
+      background-color: rgba(255, 255, 255, 0.41);
+      position: absolute;
+      left: -50%;
+      bottom: 0;
+    }
 
-  //   table {
-  //     width: 100%;
-  //     border-spacing: 0px;
-  //     border-collapse: collapse;
-  //     max-height: 308px;
-  //   }
+    th + div {
+      position: absolute;
+      left: -60%;
+      bottom: 12px;
+    }
 
-  //   thead {
-  //     height: 56px;
-  //     background-color: var(--balance-background-color);
-  //     backdrop-filter: var(--backdrop-filter);
-  //     position: sticky;
-  //     top: 0;
-  //     z-index: 2;
-  //   }
+    button.edit {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
 
-  //   tbody {
-  //     max-height: 265px;
-  //     overflow: hidden;
-  //   }
+  @media only screen and (min-width: 768px) {
+    display: block;
+    max-height: 318px;
+    overflow: auto;
+    width: 704px;
+    &::-webkit-scrollbar {
+      width: 8px;
+      border-radius: 4px;
+      background-color: #734aef;
+    }
 
-  //   th {
-  //     padding: 0;
-  //     font-weight: 600;
-  //     text-align: left;
-  //   }
-  //   th:first-child {
-  //     border-top-left-radius: 8px;
-  //     border-bottom-left-radius: 8px;
-  //     text-align: left;
-  //     padding-left: 20px;
-  //     width: 91px;
-  //   }
+    &::-webkit-scrollbar-thumb {
+      background-color: var(--balance-background-color);
+      border-radius: 4px;
+    }
 
-  //   th:nth-child(2) {
-  //     text-align: center;
-  //   }
+    .income {
+      font-weight: 600;
+      color: var(--yellow);
+      text-align: right;
+    }
+    .expense {
+      font-weight: 600;
+      color: var(--dashboard-text);
+      text-align: right;
+    }
 
-  //   th:nth-child(3) {
-  //     width: 146px;
-  //   }
+    table {
+      width: 100%;
+      border-spacing: 0px;
+      border-collapse: collapse;
+      max-height: 308px;
+    }
 
-  //   th:nth-child(4) {
-  //     width: 135px;
-  //   }
+    thead {
+      height: 56px;
+      border-radius: 8px;
+      background: rgba(82, 59, 126, 0.6);
+      box-shadow: 0px 4px 60px 0px rgba(0, 0, 0, 0.25);
+      backdrop-filter: blur(50px);
+      position: sticky;
+      top: 0;
+      z-index: 2;
+    }
 
-  //   th:nth-child(5) {
-  //     text-align: right;
-  //   }
+    tbody {
+      max-height: 265px;
+      overflow: hidden;
+    }
 
-  //   th:last-child {
-  //     border-top-right-radius: 8px;
-  //     border-bottom-right-radius: 8px;
-  //     width: 141px;
-  //   }
+    th {
+      padding: 0;
+      font-weight: 600;
+      text-align: left;
+    }
+    th:first-child {
+      border-top-left-radius: 8px;
+      border-bottom-left-radius: 8px;
+      text-align: left;
+      padding-left: 20px;
+      width: 91px;
+    }
 
-  //   tbody {
-  //     font-size: 14px;
-  //   }
+    th:nth-child(2) {
+      text-align: center;
+    }
 
-  //   tr {
-  //     position: relative;
-  //     height: 53px;
-  //   }
+    th:nth-child(3) {
+      width: 146px;
+    }
 
-  //   tr:not(:last-child)::after {
-  //     content: '';
-  //     position: absolute;
-  //     left: 0;
-  //     right: 0;
-  //     bottom: -1px;
-  //     height: 1px;
-  //     border-bottom: 1px solid white;
-  //     opacity: 0.2;
-  //   }
+    th:nth-child(4) {
+      width: 135px;
+    }
 
-  //   td {
-  //     text-align: left;
-  //     padding: 0;
-  //   }
+    th:nth-child(5) {
+      text-align: right;
+    }
 
-  //   td:first-child {
-  //     padding-left: 20px;
-  //   }
+    th:last-child {
+      border-top-right-radius: 8px;
+      border-bottom-right-radius: 8px;
+      width: 141px;
+    }
 
-  //   td:nth-child(2) {
-  //     width: 123px;
-  //     text-align: center;
-  //   }
+    tbody {
+      font-size: 14px;
+    }
+    thead tr {
+      color: var(--white, #fbfbfb);
+      font-family: Poppins;
+      font-size: 16px;
+      font-weight: 600;
+    }
+    tbody tr {
+      color: var(--white, #fbfbfb);
+      font-family: Poppins;
+      font-size: 14px;
+      font-weight: 400;
+    }
 
-  //   /* td:nth-child(4) {
-  //           text-transform: capitalize;
-  //       } */
+    tr {
+      position: relative;
+      height: 53px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
 
-  //   td:last-child {
-  //     text-align: right;
-  //     padding-right: 6px;
-  //   }
+    tr:not(:last-child)::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: -1px;
+      height: 1px;
+      border-bottom: 1px solid white;
+      opacity: 0.2;
+    }
 
-  //   button.edit {
-  //     width: 14px;
-  //     height: 14px;
-  //     margin-right: 8px;
-  //   }
-  // }
+    td {
+      text-align: left;
+      padding: 0;
+    }
 
+    td:first-child {
+      padding-left: 20px;
+    }
 
+    td:nth-child(2) {
+      width: 123px;
+      text-align: center;
+    }
+
+    /* td:nth-child(4) {
+            text-transform: capitalize;
+        } */
+
+    td:last-child {
+      text-align: right;
+      padding-right: 6px;
+    }
+
+    div > button {
+      background-color: transparent;
+      border: none;
+      fill: white;
+    }
+
+    button + button {
+      margin-left: 8px;
+      border: none;
+      border-radius: 20px;
+      background: var(
+        --button-gradient,
+        linear-gradient(97deg, #ffc727 -16.42%, #9e40ba 97.04%, #7000ff 150.71%)
+      );
+      box-shadow: 1px 9px 15px 0px rgba(0, 0, 0, 0.2);
+    }
+
+    button.edit {
+      width: 14px;
+      height: 14px;
+      margin-right: 8px;
+    }
+  }
 `;

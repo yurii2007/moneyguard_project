f4e53cb2d9743bf1ad1f0d8779ed55b8d2b73d33
yurii2007 <135147654+yurii2007@users.noreply.github.com>
2023-10-01 20:51:06 +0000

    Deploying to gh-pages from @ yurii2007/moneyguard_project@04213c37e15f4e07e6d4f109eabbfe4370ef1455 🚀

---
+++ static/js/27.205e351b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmoneyguard_project=self.webpackChunkmoneyguard_project||[]).push([[27],{3027:function(e,r,u){u.r(r);var n=u(2791),s=u(7689),t=u(4805),a=u(4393),c=u(7621),f=u(184);r.default=function(){var e=(0,t.useMediaQuery)({query:"(max-width: 767.9px)"}),r=(0,s.s0)();return(0,n.useEffect)((function(){e||r("/",{replace:!0})})),(0,f.jsxs)(f.Fragment,{children:[(0,f.jsx)(c.Z,{}),(0,f.jsx)(a.TC,{}),(0,f.jsx)(a.x1,{})]})}}}]);
+//# sourceMappingURL=27.205e351b.chunk.js.map

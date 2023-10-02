9074902e49b6af1af76cbb191e40bbc3b45b24e4
yurii2007 <yurash862@gmail.com>
2023-10-02 18:46:20 +0300

    start work with checkbox

--- src/components/TransactionsList/AddModal/Checkbox/CheckBox.styled.js
+++ src/components/TransactionsList/AddModal/Checkbox/CheckBox.styled.js
@@ -2,19 +2,48 @@ import styled from 'styled-components';
 
 export const TogglerStyles = styled.div`
   display: flex;
+  align-items: center;
   justify-content: center;
-  gap: 12px;
-
-  color: var(--white-60, rgba(255, 255, 255, 0.6));
-  font-family: Poppins;
+  gap: 20px;
   font-size: 16px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 1.5;
+  font-weight: 600;
+  line-height: 24px;
+  .secondary-text {
+    color: var(--white-color);
+  }
+  .active-type-text {
+    color: var(--yellow);
+  }
+  label {
+    position: relative;
+    width: 80px;
+    height: 40px;
+    border-radius: 30px;
+    background: var(--white-color);
+    svg {
+      position: absolute;
+      background: var(--yellow);
+      border-radius: 50%;
+      top: -2px;
+    }
+    .minus {
+      display: ${props => (props.active === 'true' ? '' : 'none')};
+    }
+    .plus {
+      display: ${props => (props.active === 'true' ? 'none' : '')};
+    }
+  }
 
-  margin-bottom: 42px;
-
-  &:focus {
-    color: var(--dashboard-text, #ff868d);
+  input {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    border: 0;
+    padding: 0;
+    white-space: nowrap;
+    clip-path: inset(100%);
+    clip: rect(0 0 0 0);
+    overflow: hidden;
   }
 `;
--- src/components/TransactionsList/AddModal/Checkbox/Checkbox.jsx
+++ src/components/TransactionsList/AddModal/Checkbox/Checkbox.jsx
@@ -7,14 +7,18 @@ export const Checkbox = ({ ...props }) => {
   const [field] = useField(props);
 
   return (
-    <TogglerStyles>
-      <p className={`${checked ? '' : 'active-type-text'}`}>Income</p>
+    <TogglerStyles active={checked.toString()}>
+      <p className={`${checked ? 'secondary-text' : 'active-type-text'}`}>
+        Income
+      </p>
       <label htmlFor="type">
-        <input {...field} {...props} />
-        <CiCirclePlus />
-        <CiCircleMinus />
+        <input id='type' {...field} {...props} />
+        <CiCirclePlus className='plus' size={44} />
+        <CiCircleMinus className='minus'  size={44} />
       </label>
-      <p className={`${checked ? 'active-type-text' : ''}`}>Expense</p>
+      <p className={`${checked ? 'active-type-text' : 'secondary-text'}`}>
+        Expense
+      </p>
     </TogglerStyles>
   );
 };
